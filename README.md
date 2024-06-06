2edaa0d143c1a4e47b67e626112f3666a5f85f05
minseok0809 <miseokyang0809@gmail.com>
2024-06-06 15:27:21 +0900

    upload files

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # image-LaTeX-extractor
-이미지에서 LaTeX를 추출하는 프로그램
+
+<br>
+
+![image-LaTeX-extractor](https://github.com/minseok0809/image-LaTeX-extractor/assets/97289420/31741340-3a7f-4006-bca5-66b5208cfa53)
+
+<br><br>
+
+## Turn Your Python Script into Executable File
+```
+pyinstaller -w image-LaTeX-extractor.py
+```
+
+<br><br>
+
+## Library
+`Tkinter` `pix2tex` `Pillow` `webbrowser` `clipboard` `PyInstaller` 
+
+<br><br>
+
+## Acknowledgment
+lukas-blecher
+<br>[LaTeX-OCR](https://github.com/lukas-blecher/LaTeX-OCR)
+
+<br><br>
+
+## Reference
+Paper
+<br>[Attention Is All You Need](https://arxiv.org/abs/1706.03762)
+<br>[Image-to-Markup Generation with Coarse-to-Fine Attention](https://arxiv.org/abs/1609.04938v2)
+<br>[An Image is Worth 16x16 Words: Transformers for Image Recognition at Scale](https://arxiv.org/abs/2010.11929)
+
+<br><br>Blog
+<br>[흔한 공대생의 일상기록: Python Tkinter - BMI 계산기 만들기](https://commonengineerr.tistory.com/7)
+
+<br><br>
---
+++ image-LaTeX-extractor.py
@@ -0,0 +1,50 @@
+import clipboard
+from tkinter import *
+import tkinter.filedialog as fd
+from pix2tex.cli import LatexOCR
+from PIL import Image
+import webbrowser
+
+
+def callback(url):
+   webbrowser.open_new_tab(url)
+
+def one():
+   file_path = fd.askopenfilename()
+   file_path = str(file_path)
+   model = LatexOCR()
+   with Image.open(file_path) as img:
+      latex_text = model(img)
+   img.close()
+   clipboard.copy(latex_text)
+   e1.delete(0, len(e1.get()))             
+   e1.insert(0, latex_text)  
+
+
+window = Tk()
+window.title('Image LaTeX Extractor')
+window_width = 600
+window_height = 300
+window_pos_x = 500
+window_pos_y = 100
+window.geometry("{}x{}+{}+{}".format(window_width, window_height, window_pos_x, window_pos_y))
+window.resizable(width=False, height=False)
+
+l1 = Label(window, text = 'Image LaTeX Extractor', fg = 'orange', font = 'helvetica 16 bold')
+l1.pack()
+l1.place(x=20, y=10)
+
+e1 = Entry(window)
+e1.pack()
+e1.place(x=20, y=60, width=400, height=50)
+
+b1 = Button(window, text = "Load Image File", command=one)  
+b1.pack()
+b1.place(x=20, y=140)
+
+
+l2 = Label(window, text = 'Acknowledgment: lukas-blecher/LaTeX-OCR', fg = 'black', font = 'helvetica 12 bold')
+l2.pack()
+l2.place(x=20, y=200)
+l2.bind("<Button-1>", lambda e: callback("https://github.com/lukas-blecher/LaTeX-OCR"))
+window.mainloop()
